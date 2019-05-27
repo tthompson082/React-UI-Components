b63d16b5931fc562deb9466fd39ae5be2d8d58f6
Tyler Thompson <tylerthompson@Tylers-MacBook-Pro.local>
2019-05-27 12:20:12 -0400

    added header components and part of card components

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,2 +1,9 @@
 import React from 'react';
 import './Card.css';
+
+
+const CardBanner = () => (
+    <img className="card-banner" src="https://tk-assets.lambdaschool.com/fcd75197-7d12-46ec-bc9e-4130f34822fa_reactbackground.png" />
+);
+
+export default CardBanner;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Header.css';
+import ImageThumbnail from './ImageThumbnail';
+import HeaderTitle from './HeaderTitle';
+import HeaderContent from './HeaderContent';
+
+const HeaderContainer = () => (
+    <div className="header-container">
+        <ImageThumbnail />;
+        <HeaderTitle />;
+        <HeaderContent />;
+    </div>
+);
